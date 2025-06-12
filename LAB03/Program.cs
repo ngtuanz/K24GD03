fc663d664e09445774be8c6a0f1560582f8ac165
ngtuanz <nguyenanhtuan4643@gmail.com>
2025-06-12 11:37:18 +0700

    LAB03
    main

---
+++ LAB03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+
+namespace LAB03
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.Write("Number: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                rnd.RandomNum(n);
+            }
+            else
+            {
+                Console.WriteLine("Number > 0.");
+            }
+        }
+    }
+}
---
+++ LAB03/Random.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB03
+{
+    public class Random
+    {
+        public void RandomNum(int n)
+        {
+            Console.WriteLine("Value: " + n);
+
+            System.Random random = new System.Random();
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = random.Next(100);
+                numbers.Add(value);
+            }
+
+            numbers.Sort();
+
+            Console.WriteLine("List:");
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
